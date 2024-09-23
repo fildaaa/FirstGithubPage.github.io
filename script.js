80a9dfbc70cda6adadf29dfe583aab667423db6c
fildaaa <fildamarsh@gmail.com>
2024-09-23 20:50:26 +0700

    something missing

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ tombol.addEventListener("click", () => {
     console.log('clicked');
     const hello = document.getElementById("hello");
     hello.style.backgroundColor = "black";
-    hello.style.color = "white"
+    hello.style.color = "white";
 
     let list = document.getElementById('list');
     let content = '';
